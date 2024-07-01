dfcf8d96937a21bdb81122e4e9115eadf70c1014
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-06-30 22:11:39 -0300

    wip: icpc sr 2023 (almost there)

--- icpc/2023-regional/e-true.cpp
+++ icpc/2023-regional/e-true.cpp
@@ -25,7 +25,7 @@ void printArray(long long arr[], long long size) {
 
 void printBirdBox(long long arr[], long long size, ll i) {
     std::cout << "[ ";
-    for (ll i=size-1; i>=0; i--)
+    for (ll i=0; i<size; i++)
         if (arr[i] > 0)
             for (ll k = arr[i]; k>0; k--)
                 std::cout << i << ", " ;
@@ -47,20 +47,51 @@ int main() {
         flowers[flower]++;
     }
 
+    ll tillGertrude = gertrudePosition-1;
+
     for (long long i=flowersMaxQtd-1; i>=0; i--) {
-        //if (i<40)
-        //    printBirdBox(flowers, 40, i);
 
         ll minus = get_sum_of_digits(i);
         ll result = i - minus;
 
-        while (flowers[i] > 0) { //caso o pólen da flor atual seja maior que zero
-            flowers[result]++; // aumenta a quantidade de flores com o pólen restante
-            flowers[i]--; // diminui a quantidade de flores com o pólen da flor atual
-            bees++;
-            if (bees == gertrudePosition) { std::cout << minus <<"\n"; return 0; }
-            //std::cout << "sum of digits: " << minus << "i: " << i << "\n";
+        
+        if (flowers[i] > tillGertrude || ((tillGertrude == 0) && (flowers[i]>0)) ) { 
+            /* BASICAMENTE EU NÃO POSSO RETORNAR SÓ POR TILLGERTRUDE SER 0 POIS SE NAO ELE VAI RETORNAR SEM TER NENHUMA FLOR
+            (A PROXIMA POSIÇAO INDEPENDENTE DAS VARIAVEIS, NO CASO DO 1 EXEMPLO EH NA POSIÇÃO 17 (DPS DA 18, QUE EH UMA FLOR QUE EXISTE)) */
+            printArray(flowers, 26);
+            std::cout << "gertrude menos " << flowers[i] << ", qtd: " << flowers[i] << ", tillGertrude = " << tillGertrude << ", i = " << i << "\n";
+            std::cout << minus << "\n"; break; return 0;
         }
+        tillGertrude -= flowers[i];
+        if (flowers[i]>0) {
+            printArray(flowers, 26);
+            std::cout << "gertrude menos " << flowers[i] << ", qtd: " << flowers[i] << ", tillGertrude = " << tillGertrude << ", i = " << i << "\n";
+        }
+        flowers[result] += flowers[i];
+        flowers[i] = 0;
+        
+
+        // if ((flowers[i] >= tillGertrude) || (tillGertrude == 1)) {
+        //     //std::cout << "qtd de flores com polen i maior que qtd de abelhas até gertrude\n";
+        //     flowers[i] -= tillGertrude; // simbólico
+        //     std::cout << minus <<"\n";
+        //     std::cout << "i = " << i << ", minus = " << minus << ", qtd: " << flowers[i] << " GERTRUDE UAI TCHÊ MÊO\n";
+        //     break;
+        // } else 
+        // if ((flowers[i] < tillGertrude) && flowers[i]>0) { // caso haja mais flores até gertrude do que flores com o tanto de pólen atual..
+        //     //std::cout << "posição de gertrude maior que a qtd de flores\n";
+        //     tillGertrude -= flowers[i];
+        //     std::cout << "gertrude menos " << flowers[i] << ", qtd: " << flowers[i] << ", gertrude = " << tillGertrude << "\n";
+        //     flowers[result] + flowers[i];
+        //     flowers[i] = 0;
+        // }
+
+        // while (flowers[i] > 0) { //caso o pólen da flor atual seja maior que zero
+        //     flowers[result]++; // aumenta a quantidade de flores com o pólen restante
+        //     flowers[i]--; // diminui a quantidade de flores com o pólen da flor atual
+        //     bees++;
+        //     if (bees == gertrudePosition) { std::cout << minus <<"\n"; return 0; }
+        //     //std::cout << "sum of digits: " << minus << "i: " << i << "\n";
+        // }
     }
-    
 }
